b2fff3799e6914d96999c56716cdaea8f32e516e
Wilmer Rodriguez <wilmerr09@gmail.com>
2023-03-01 20:43:38 -0300

    fix colors buttons

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div className="text-white absolute inset-0 flex flex-col justify-center items-center">
       <CounterText count={count}/>
       <div className="flex gap-2">
-        <CounterButton textButton="Descrementar" onClick={() => {setCount(count - 1)}}/>
+        <CounterButton textButton="Descrementar" style={"bg-purple-600 shadow-purple-500"} onClick={() => {setCount(count - 1)}}/>
         <CounterButton textButton="Incrementar" style={"bg-green-600 shadow-green-500"} onClick={() => {setCount(count + 1)}}/>
       </div>
     </div>
--- src/components/CounterButton.jsx
+++ src/components/CounterButton.jsx
@@ -4,7 +4,7 @@ function CounterButton({textButton, onClick, style}) {
 
   return (
     <div>
-      <button className={`${style} border-transparent rounded-full px-2 py-1 bg-purple-600 shadow-purple-500 shadow-md`} onClick={onClick}>{textButton}</button>
+      <button className={`${style} border-transparent rounded-full px-2 py-1 shadow-md`} onClick={onClick}>{textButton}</button>
     </div>
   )
 }
